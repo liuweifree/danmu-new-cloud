ce4f4b4c1db1efc0555e08c19f327bacc283ed40
liuwei <lw6866633@sina.com>
2015-12-22 12:36:23 +0800

    add config-repo

--- danmu-config-server/src/test/java/com/acfun/DanmuConfigServerApplicationTests.java
+++ danmu-config-server/src/test/java/com/acfun/DanmuConfigServerApplicationTests.java
@@ -2,15 +2,40 @@ package com.acfun;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DanmuConfigServerApplication.class)
 public class DanmuConfigServerApplicationTests {
 
+	@Value("${local.server.port}")
+	private int port = 0;
+
 	@Test
-	public void contextLoads() {
+	public void configurationAvailable() {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/app/cloud", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void envPostAvailable() {
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+				"http://localhost:" + port + "/admin/env", form, Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
 }
